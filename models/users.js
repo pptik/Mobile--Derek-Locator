155358207c2e6ec7a7775e4b06ad64cb9b3f1e34
Hendra Permana <hynra@yahoo.com>
2016-09-22 11:17:19 +0700

    Add signup rest

--- models/users.js
+++ models/users.js
@@ -33,3 +33,66 @@ db.open(function(e, d){
         }
     }
 });
+
+var users = db.collection('users');
+
+/* insert akun */
+
+addNewAccount = function(newData, callback)
+{
+    users.findOne({user:newData.user}, function(e, o) {
+        if (o){
+            callback('Maaf, username telah digunakan');
+            console.log('user : '+newData.user+' is used');
+        }	else{
+            users.findOne({email:newData.email}, function(e, o) {
+                if (o){
+                    console.log('email : '+newData.email+' is used');
+                    callback('Maaf, email sudah digunakan');
+                }else{
+                    saltAndHash(newData.pass, function(hash){
+                        newData.pass = hash;
+                        newData.date = moment().format('MMMM Do YYYY, h:mm:ss a');
+                        newData.isVerified = false;
+                        users.insert(newData, {safe: true}, callback);
+                    });
+                }
+            });
+        }
+    });
+}
+
+
+/* salt */
+var validatePassword = function(plainPass, hashedPass, callback)
+{
+    var salt = hashedPass.substr(0, 10);
+    var validHash = salt + md5(plainPass + salt);
+    callback(null, hashedPass === validHash);
+}
+
+var generateSalt = function()
+{
+    var set = '0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ';
+    var salt = '';
+    for (var i = 0; i < 10; i++) {
+        var p = Math.floor(Math.random() * set.length);
+        salt += set[p];
+    }
+    return salt;
+}
+
+var md5 = function(str) {
+    return crypto.createHash('md5').update(str).digest('hex');
+}
+
+var saltAndHash = function(pass, callback)
+{
+    var salt = generateSalt();
+    callback(salt + md5(pass + salt));
+}
+
+
+module.exports = {
+    addNewAccount: addNewAccount
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../models/users.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+
+router.post('/signup', function(req, res){
+    users.addNewAccount({
+        name 	: req.body['name'],
+        email 	: req.body['email'],
+        user 	: req.body['user'],
+        pass	: req.body['pass'],
+        role    : req.body['role'],
+        id_unit : req.body['id_unit']
+    }, function(e, o){
+        var dataRes = {};
+        var status = {};
+        if (e){
+            status.code = 400;
+            status.success = false;
+            status.msg = e;
+            dataRes.status = status;
+            res.status(400).send(dataRes);
+        }	else{
+            status.code = 200;
+            status.success = true;
+            status.msg = 'Sukses membuat akun';
+            dataRes.status = status;
+            dataRes.data = o.ops[0];
+            res.status(200).send(dataRes);
+
+        }
+    });
 });
 
 module.exports = router;
