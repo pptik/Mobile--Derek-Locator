ccfa87421337d2c2a62990c6f959451cc92175e9
Hendra Permana <hynra@yahoo.com>
2016-09-22 14:24:55 +0700

    Move mongodb conn to configs.js

---
+++ models/config.js
@@ -0,0 +1,37 @@
+/**
+ * Created by hynra on 9/22/16.
+ */
+
+var MongoDB 	= require('mongodb').Db;
+var Server 		= require('mongodb').Server;
+
+//---- mongodb var
+
+/* Todo: ubah dengan db live*/
+var dbName = process.env.DB_NAME || 'semut_demo';
+var dbHost = process.env.DB_HOST || 'MacBookPro.local'
+var dbPort = process.env.DB_PORT || 27017;
+
+var db = new MongoDB(dbName, new Server(dbHost, dbPort, {auto_reconnect: true}), {w: 1});
+db.open(function(e, d){
+    if (e) {
+        console.log(e);
+    } else {
+        if (process.env.NODE_ENV == 'live') {
+            db.authenticate(process.env.DB_USER, process.env.DB_PASS, function(e, res) {
+                if (e) {
+                    console.log('mongo : error: not authenticated', e);
+                }
+                else {
+                    console.log('mongo : authenticated and connected to database :: "'+dbName+'"');
+                }
+            });
+        }	else{
+            console.log('mongo : connected to database :: "'+dbName+'"');
+        }
+    }
+});
+
+module.exports = {
+    db: db
+};
--- models/users.js
+++ models/users.js
@@ -3,38 +3,10 @@
  */
 
 var crypto 		= require('crypto');
-var MongoDB 	= require('mongodb').Db;
-var Server 		= require('mongodb').Server;
 var moment 		= require('moment');
+var configs     = require('./config');
 
-//---- mongodb var
-
-/* Todo: ubah dengan db live*/
-var dbName = process.env.DB_NAME || 'semut_demo';
-var dbHost = process.env.DB_HOST || 'MacBookPro.local'
-var dbPort = process.env.DB_PORT || 27017;
-
-var db = new MongoDB(dbName, new Server(dbHost, dbPort, {auto_reconnect: true}), {w: 1});
-db.open(function(e, d){
-    if (e) {
-        console.log(e);
-    } else {
-        if (process.env.NODE_ENV == 'live') {
-            db.authenticate(process.env.DB_USER, process.env.DB_PASS, function(e, res) {
-                if (e) {
-                    console.log('mongo : error: not authenticated', e);
-                }
-                else {
-                    console.log('mongo : authenticated and connected to database :: "'+dbName+'"');
-                }
-            });
-        }	else{
-            console.log('mongo : connected to database :: "'+dbName+'"');
-        }
-    }
-});
-
-var users = db.collection('users');
+var users = configs.db.collection('users');
 
 /* insert akun */
 
