b41476807782efd8c70ec561d1b763abdf23d764
Hendra Permana <hynra@yahoo.com>
2016-10-04 11:44:14 +0700

    Add map layout

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,9 @@ router.get('/dashboard', function(req, res, next) {
   res.render('dashboard-v1', { title: 'Express' });
 });
 
+/* GET dashboard. */
+router.get('/map', function(req, res, next) {
+    res.render('maps', { title: 'Express' });
+});
+
 module.exports = router;
