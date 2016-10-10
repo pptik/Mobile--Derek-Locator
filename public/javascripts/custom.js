8f607ccd8c91b20ff05ce393d95fd6da7b3743cb
emilhamep <emilhamep@gmail.com>
2016-10-10 16:00:32 +0700

    Update untuk bagian map. Penampilan interface driver dan report.

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -35,6 +35,8 @@ map = {
                         '</div>';
                     infowindow.setContent(contentString);
                     infowindow.open(map, marker);
+                    alert('asoy');
+
                 }
             })(marker, i));
         }
@@ -64,7 +66,7 @@ map = {
         });
 
         marker.addListener('click', function () {
-            infowindow.open(map, marker);
+            infowindow.open(map, marker)
         });
 
         var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
